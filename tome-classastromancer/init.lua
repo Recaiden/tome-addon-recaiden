2837a47fd3d9519f6965abc6d97fe95bcbf60320
Recaiden <mlwatts45@gmail.com>
2019-11-22 00:14:07 -0600

    Version bump, new graphics.

--- tome-classastromancer/init.lua
+++ tome-classastromancer/init.lua
@@ -2,7 +2,7 @@ long_name = "Class: Astromancer"
 short_name = "classastromancer"
 for_module = "tome"
 version = {1,5,0}
-addon_version = {1, 3, 0}
+addon_version = {1, 4, 0}
 weight = 27691
 author = { 'recaiden' }
 description = [[A new class, a Celestial summoner who draws on the powers of Shandral's other planets to call forth elemental forces.]]
@@ -16,7 +16,5 @@ data = true
 -- Sound sampled from clips by Mike Koenig, available under Creative Commons Attribution 3.0 https://creativecommons.org/licenses/by/3.0/us/
 
 --TODO egos, items that only spawn if Astromancer?
---Double check item spawning
---TODO more graphics
 
 
