a03a6c2a9ced3bb7df9237f6e24bcc77ef495963
Recaiden <mlwatts45@gmail.com>
2021-05-31 17:59:19 -0500

    Evolutions
    Hollow's shadows level up normally.

--- tome-rec-evolutions/superload/data/talents/cursed/shadows.lua
+++ tome-rec-evolutions/superload/data/talents/cursed/shadows.lua
@@ -2,7 +2,7 @@ local _M = loadPrevious(...)
 
 local baseCSDeactivate = Talents.talents_def.T_CALL_SHADOWS.deactivate
 Talents.talents_def.T_CALL_SHADOWS.deactivate = function(self, t)
-	if self:knowTalent(self.T_REK_EVOLUTION_DOOMED_HOLLOW) then
+	if self:knowTalent(self.T_REK_EVOLUTION_DOOMED_HOLLOW) and not (self.turn_procs and self.turn_procs.resetting_talents) then
 		game.logPlayer(self, "The shadow call can no longer be stopped.")
 		return nil
 	end
