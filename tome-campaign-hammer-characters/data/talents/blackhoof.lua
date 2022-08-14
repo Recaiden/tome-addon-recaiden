df337aea6504c1b627233b0631ae0ce2b04411ac
Recaiden <mlwatts45@gmail.com>
2022-08-14 18:14:34 -0500

    Hammer campaign character options - blackhoof talent definitions but not effects.

--- tome-campaign-hammer-characters/data/talents/blackhoof.lua
+++ tome-campaign-hammer-characters/data/talents/blackhoof.lua
@@ -1,6 +1,6 @@
 newTalent{
-	name = "Impulse", short_name="REK_ONYX_IMPULSE",
-	type = {"race/onyx", 1}, require = racial_req1, points = 5,
+	name = "Blackhooves", short_name="REK_BLACKHOOF_MOVEMENT",
+	type = {"race/blackhoof", 1}, require = racial_req1, points = 5,
 	tactical = { CURE = 1 },
 	no_energy = true,
 	cooldown = function(self, t) return math.ceil(self:combatTalentLimit(t, 10, 40, 12)) end,
@@ -26,11 +26,11 @@ newTalent{
 		end
 
 		-- move fast
-		self:setEffect(self.EFF_REK_ONYX_IMPULSE, 1, {steps=t.getSteps(self, t), src=self})
+		self:setEffect(self.EFF_REK_BLACKHOOF_IMPULSE, 1, {steps=t.getSteps(self, t), src=self})
 
 		-- check talent 3
-		if self:knowTalent(self.T_REK_ONYX_TACTICAL) then
-			self.energy.value = self.energy.value + game.energy_to_act * self:callTalent(self.T_REK_ONYX_TACTICAL, "getEnergy")
+		if self:knowTalent(self.T_REK_BLACKHOOF_TACTICAL) then
+			self.energy.value = self.energy.value + game.energy_to_act * self:callTalent(self.T_REK_BLACKHOOF_TACTICAL, "getEnergy")
 		end
 		return true
 	end,
@@ -40,70 +40,87 @@ newTalent{
 }
 
 newTalent{
-	name = "Devil Spines", short_name = "REK_ONYX_SPINES",
-	type = {"race/onyx", 2}, require = racial_req2, points = 5,
+	name = "Wreck", short_name = "REK_BLACKHOOF_WRECK",
+	type = {"race/blackhoof", 2}, require = racial_req2, points = 5,
 	mode = "passive",
 	range = 3,
-	getHardiness = function(self, t) return self:combatTalentScale(t, 10, 25) end,
-	getDamage = function(self, t)
-		return 5 + self:combatArmor() / 2
-	end,
-	passives = function(self, t, p)
-		self:talentTemporaryValue(p, "combat_armor_hardiness", t.getHardiness(self, t))
-	end,
-	callbackOnHit = function(self, t, cb, src, death_note)
-		self.rek_onyx_spines_proc = true
-		return cb
-	end,
-	callbackOnActBase = function(self, t)
-		if not self.rek_onyx_spines_proc and not self.rek_onyx_spines_autoproc then return end
-		self.rek_onyx_spines_proc = nil
-		local tg = {type="ball", range=0, radius=self:getTalentRange(t), friendlyfire=false, talent=t}
-		self:project(tg, self.x, self.y, DamageType.PHYSICAL, t.getDamage(self, t) + (self.rek_onyx_spines_autoproc or 0))
-	end,
-	info = function(self, t)
-		return ([[At the start of every round, if you were hit in the last round, lash out with your spines, dealing %0.1f physical daamge (based on your armor) to enemies within range %d. 
-
-You also gain %d%% armor hardiness.
-
-#{italic}#....#{normal}#]]):tformat(damDesc(self, DamageType.PHYSICAL, t.getDamage(self, t)), self:getTalentRange(t), t.getHardiness(self, t))
-	end,
-}
-
-newTalent{
-	name = "Principle of Motion", short_name = "REK_ONYX_TACTICAL",
-	type = {"race/onyx", 3}, require = racial_req3,	points = 5,
-	mode = "passive",
-	getEnergy = function(self, t) return self:combatTalentScale(t, 0.2, 0.5) end,
-	info = function(self, t)
-		return ([[Making efficient use of every movement, you are capable of sudden bursts of speed.  Whenever you use Impulse or Unbreakable, you gain %d%% of a turn.
-
-#{italic}#More than any other demons, the children of onyx are warriors at heart.#{normal}#]]):tformat(t.getEnergy(self, t)*100)
-	end,
-}
-
-newTalent{
-	name = "Unbreakable", short_name = "REK_ONYX_UNBREAKABLE",
-	type = {"race/onyx", 4}, require = racial_req4, points = 5,
-	no_energy = true,
-	cooldown = function(self, t) return math.ceil(self:combatTalentLimit(t, 5, 45, 25)) end, -- Limit > 5
-	getDuration = function(self, t) return 5 end,
-	getPower = function(self, t) return self:combatStatScale("cun", 11, 25) end,
-	getDamage = function(self, t) return self:getCun() * self:combatTalentScale(t, 0.5, 1.25) end,
-	tactical = { ATTACK = 1, DEFEND = 1 },
-	action = function(self, t)
-		self:setEffect(self.EFF_REK_ONYX_UNBREAKABLE, 5, {power=t.getPower(self, t), damage=getDamage(self,t), src=self})
-		
-		-- check talent 3
-		if self:knowTalent(self.T_REK_ONYX_TACTICAL) then
-			self.energy.value = self.energy.value + game.energy_to_act * self:callTalent(self.T_REK_ONYX_TACTICAL, "getEnergy")
+	getMaxStacks = function(self, t) return math.floor(self:getTalentLevel(t)) end,
+	getPower = function(self, t) return self:combatScale(self.level, 1, 8, 2, 50) * 10 end,
+	callbackOnDealDamage = function(self, t, val, target, dead, death_note)
+		if dead then return end
+		if self.turn_procs.rek_blackhoof then
+			for i = 1, #self.turn_procs.rek_blackhoof_wreck do
+	      if self.turn_procs.rek_blackhoof_wreck[i] == target.uid then return end
+			end
 		end
+		self.turn_procs.rek_blackhoof_wreck = self.turn_procs.rek_blackhoof_wreck or {}
+		self.turn_procs.rek_blackhoof_wreck[#self.turn_procs.rek_blackhoof_wreck+1] = target.uid
+		target:setEffect(target.EFF_REK_BLACKHOOF_WRECK, 5, {max_stacks = t.maxStacks(self,t)}, true)
+	end,
+	info = function(self, t)
+		return ([[Each time you damage an opponent (once per turn per target), you reduce their defense and saves by %d for 5 turns.
+This stacks up to %d times.
+
+#{italic}#....#{normal}]]):tformat(t:_getPower(self), t:-maxStacks(self))
+	end,
+}
+
+newTalent{
+	name = "Bewildering Roar", short_name = "REK_BLACKHOOF_CONFUSE",
+	type = {"race/blackhoof", 3}, require = racial_req3,	points = 5,
+	cooldown = function(self, t) return math.ceil(self:combatTalentLimit(t, 9, 46, 30)) end,
+	tactical = { ATTACKAREA = {PHYSICAL=2}, DISABLE = {confusion=2} },
+	getDamage = function(self, t) return self:combatStatScale("con", 50, 300) end,
+	getDur = function(self, t) return self:combatTalentLimit(t, 8, 3, 5) end,
+	radius = function(self, t) return self:gatTalentRange(t) end,
+	range = 5,
+	target = function(self, t) return {type="cone", radius=self:getTalentRadius(t), range=self:getTalentRange(t), friendlyfire=false} end,
+	requires_target = true,
+	action = function(self, t)
+		local tg = self:getTalentTarget(t)
+		self:project(tg, self.x, self.y, DamageType.PHYSICAL, self:physicalCrit(t.getDamage(self, t)))
+		self:project(tg, self.x, self.y, function(px, py)
+			local target = game.level.map(px, py, Map.ACTOR)
+			if not target then return end
+			if target:canBe("confusion") then
+				target:setEffect(target.EFF_CONFUSED, t.getDur(self, t), {power=35, apply_power=self:combatBestpower()})
+			end
+		end)
+		game.level.map:particleEmitter(self.x, self.y, tg.radius, "gravity_spike", {radius=tg.radius, allow=core.shader.allow("distort")})
 		return true
 	end,
 	info = function(self, t)
-		return ([[Harden your skin and sprout additional spines.  For the next %d turns, you gain %d%% absolute resistance, and Devil Spines always triggers (with %0.1f additional damage).
-Cunning: increases resistance and damage.
+		return ([[Let out a deafening roar that covers a cone of radius %d.  Anyone caught in the area will suffer %0.2f physical damage and be confused (#SLATE#35%% confusion, best power vs mental save#LAST#) for %d turns.  
+The damage will increase with your Constitution.]]):tformat(self:getTalentRadius(t), damDesc(self, DamageType.PHYSICAL, t.getDamage(self, t)), t.getDur(self, t))
+	end,
 
-#{italic}#...#{normal}#]]):format(t.getDuration(self, t), t.getPower(self, t), damDesc(self, DamageType.PHYSICAL, t.getDamage(self, t)))
+
+	
+	info = function(self, t)
+		return ([[Making efficient use of every movement, you are capable of sudden bursts of speed.  Whenever you use Impulse or Unbreakable, you gain %d%% of a turn.
+
+#{italic}#More than any other demons, the children of blackhoof are warriors at heart.#{normal}#]]):tformat(t.getEnergy(self, t)*100)
+	end,
+}
+
+newTalent{
+	name = "Essence Surge", short_name = "REK_BLACKHOOF_POWERUP",
+	type = {"race/blackhoof", 4}, require = racial_req4, points = 5,
+	mode = "passive",
+	getDamage = function(self, t) return self:combatScale(self.level, 1, 24, 2, 50) * (10 + self:combatTalentScale(t, 0, 15)) end, -- up to 50% at 5/5 and level 50
+	callbackOnMove = function(self, eff, moved, force, ox, oy, x, y)
+		if not moved then return end
+		if force then return end
+		if ox == self.x and oy == self.y then return end
+		
+		if not self:hasEffect(self.EFF_REK_BLACKHOOF_MOVEMENT) and self:isTalentCoolingDown(self.T_REK_BLACKHOOF_MOVEMENT) then
+			self:alterTalentCoolingDown(self.T_REK_BLACKHOOF_MOVEMENT, -1)
+		end
+		self:setEffect(self.EFF_BLACKHOOF_POWERUP, 2, {power=t:_getDamage(self), src=self})
+	end,
+	info = function(self, t)
+		return ([[Each time you move in combat, you build up destructive magical energies, giving you +d%% damage for 2 turns and reducing the cooldown of Blackhooves by 1 if it is not already active.
+
+#{italic}#...#{normal}#]]):format(t.getDamage(self, t))
 	end,
 }
